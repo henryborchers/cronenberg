38e655196a1c70b4c2386c39f3f68286f382e439
henryborchers@yahoo.com <henryborchers@yahoo.com>
2021-08-01 14:10:12 -0500

    map and dups have subcommands

--- cronenberg/scanner.py
+++ cronenberg/scanner.py
@@ -51,27 +51,88 @@ class PathScanner:
                 yield file_path
 
 
+class CommandParserBuilder(abc.ABC):
+    def __init__(self, root):
+        self.root = root
+
+    @abc.abstractmethod
+    def build_command_subparser(self) -> argparse.ArgumentParser:
+        """Build and return newly create ArgumentParser"""
+
+
+class MapParserBuilder(CommandParserBuilder):
+
+    @classmethod
+    def _build_create_command(cls, map_command_parser):
+        create_command = map_command_parser.add_parser("create")
+
+        create_command.add_argument("root", help="starting point")
+
+        create_command.add_argument("outputfile",
+                                    help="database file to store to")
+
+        create_command.add_argument('--suppression_file',
+                                    default=None,
+                                    help="Json file for suppressing searches")
+
+        create_command.add_argument("--append", default=False,
+                                    action='store_true')
+
+    def build_command_subparser(self) -> argparse.ArgumentParser:
+        map_parser = self.root.add_parser(
+            "map",
+            help="make a mapping of a file path"
+        )
+
+        map_command_parser = map_parser.add_subparsers(dest="map_commands")
+        self._build_create_command(map_command_parser)
+        return map_parser
+
+
+class DupsParserBuilder(CommandParserBuilder):
+
+    @classmethod
+    def _build_locate_command(cls, command_parser):
+        create_command = command_parser.add_parser("locate")
+
+        create_command.add_argument("root", help="starting point")
+        create_command.add_argument("--mapfile", action="extend", nargs="+",
+                                help="database file to compare against")
+        create_command.add_argument("--output_file", default=None,
+                                help="output file")
+        create_command.add_argument('--suppression_file',
+                                default=None,
+                                help="Json file for suppressing searches")
+
+    def build_command_subparser(self) -> argparse.ArgumentParser:
+        dup_parser = self.root.add_parser("dups", help="Find duplicates")
+        dup_command_parser = dup_parser.add_subparsers(dest="dups_command")
+        self._build_locate_command(dup_command_parser)
+
+        return dup_parser
+
+
+class ParserCreator:
+
+    def _build_map_parser(self, root_subparser) -> argparse.ArgumentParser:
+        builder = MapParserBuilder(root_subparser)
+        return builder.build_command_subparser()
+
+    def _build_dups_parser(self, root_subparser) -> argparse.ArgumentParser:
+        builder = DupsParserBuilder(root_subparser)
+        return builder.build_command_subparser()
+
+    def get(self) -> argparse.ArgumentParser:
+        parser = argparse.ArgumentParser()
+        subparser = parser.add_subparsers(dest='command')
+        self._build_map_parser(subparser)
+        self._build_dups_parser(subparser)
+
+        return parser
+
 def get_arg_parser():
-    parser = argparse.ArgumentParser()
-    subparser = parser.add_subparsers(dest='command')
-
-    map_parser = subparser.add_parser("map", help="make a mapping of a file path")
-    map_parser.add_argument("root", help="starting point")
-    map_parser.add_argument("outputfile", help="database file to store to")
-    map_parser.add_argument('--suppression_file',
-                            default=None,
-                            help="Json file for suppressing searchs")
-    map_parser.add_argument("--append", default=False, action='store_true')
-
-    dup_parser = subparser.add_parser("dups", help="Find duplicates")
-    dup_parser.add_argument("root", help="starting point")
-    dup_parser.add_argument("--mapfile", action="extend", nargs="+", help="database file to compare against")
-    dup_parser.add_argument("--output_file", default=None, help="output file")
-    dup_parser.add_argument('--suppression_file',
-                            default=None,
-                            help="Json file for suppressing searchs")
-
-    return parser
+    builder = ParserCreator()
+    return builder.get()
 
 
 class Command(abc.ABC):
@@ -121,7 +182,7 @@ class DupsPath(Command):
                 for f in scanner.scan_path(self.root):
                     print(f)
                     matches = [m for m in reader.find_matches(f)]
-                    if len(matches) > 0:
+                    if matches:
                         print(f"Found duplicate for {f}: {matches}", file=sys.stderr)
 
                         report_writer.add_duplicates(
@@ -201,37 +262,3 @@ def main(argv: typing.Optional[typing.List[str]] = None):
         print(f"Unknown command: {args.command}", file=sys.stderr)
         sys.exit(1)
     command(args).execute()
-    # if args.command == "map":
-        # output_files = args.outputfile
-        # if not os.path.exists(output_files):
-        #     with recorder.SQLiteWriter(
-        #             filename=output_files,
-        #             schema_strategy=recorder.DataSchema1()) as writer:
-        #         writer = typing.cast(recorder.SQLiteWriter, writer)
-        #         print("initing the tables")
-        #         writer.init_tables()
-        #
-        # with recorder.SQLiteWriter(
-        #         filename=output_files,
-        #         schema_strategy=recorder.DataSchema1()) as writer:
-        #     existing_files = set()
-        #     for i, (file_name, file_path, _) in enumerate(writer.get_records()):
-        #         existing_files.add(os.path.join(file_path, file_name))
-        #     print(f"loaded {len(existing_files)} records")
-        #     writer = typing.cast(recorder.SQLiteWriter, writer)
-        #
-        #     buffer = []
-        #     try:
-        #         for f in scan_path(args.root):
-        #             if str(f.relative_to(args.root)) in existing_files:
-        #                 print(f"Skipping {f.relative_to(args.root)}")
-        #                 continue
-        #             data = filescanner.scan_file(args.root, f)
-        #             print(f.relative_to(args.root))
-        #
-        #             buffer.append((f.name, data.path, data.size))
-        #             if len(buffer) > 100:
-        #                 writer.add_files(buffer)
-        #                 buffer.clear()
-        #     finally:
-        #         writer.add_files(buffer)
